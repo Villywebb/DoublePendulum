437d77dc5a6b7207f70285d5c967eb337472237a
Villywebb <vilmer.lundin@gmail.com>
2024-01-28 22:23:04 +0100

    init commit

--- src/main/java/edu/gyarb/doublependulum/Simulator.java
+++ src/main/java/edu/gyarb/doublependulum/Simulator.java
@@ -2,22 +2,25 @@ package edu.gyarb.doublependulum;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Simulator extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Group root = new Group();
+        Scene scene = new Scene(root, 700, 700);
+        stage.setTitle("DoublePendulum");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module edu.gyarb.doublependulum {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens edu.gyarb.doublependulum to javafx.fxml;
     exports edu.gyarb.doublependulum;
 }
